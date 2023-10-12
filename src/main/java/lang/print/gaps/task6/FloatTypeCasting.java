d3cd3e1b6cd5fe137ac458912b3cbacda6d7e620
Maryia Maksimava <maryia_maksimava@epam.com>
2023-10-12 11:49:10 +0400

    FloatTypeCasting

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -3,10 +3,11 @@ package lang.print.gaps.task6;
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
 
-        int result;
-        result = (int) numberToBeRounded;
+        double result = Math.round(numberToBeRounded);
+        int finish;
+        finish = (int) result;
 
-        System.out.println(result);
+        System.out.println(finish);
 
 
     }
